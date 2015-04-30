6f4424c1555cefb79853e49190e2a290e8b38dc5
david.um <david.um@lge.com>
2015-04-30 09:40:14 +0900

    This commit is temporal commit. Will be removed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var
 	Packager = require('./lib/Packager');
 
 /**
-* Package an Enyo 2.6+ application based on the provided options.
+* Package an Enyo 2.6.x application based on the provided options.
 */
 exports.package = function (opts) {
 	return new Packager(opts);
